f3484100ad9ba9be8706881ce7ebcb98f16b235f
Skyler Svendsen <skylersvendsen@Skylers-MBP.lan>
2017-09-13 21:29:33 -0500

    first commit

---
+++ HwSProject14/WhackSlot.swift
@@ -0,0 +1,13 @@
+//
+//  WhackSlot.swift
+//  HwSProject14
+//
+//  Created by Skyler Svendsen on 9/13/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class WhackSlot: SKNode {
+
+}
